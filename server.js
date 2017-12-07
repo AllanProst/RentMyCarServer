1e90cd1dba62f723a345c5cf9fb48425fbe84da1
Allan Prost <prost.allan89@gmail.com>
2017-12-07 16:12:38 +0100

    avec api google

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ var options = { server: { socketOptions: { connectTimeoutMS: 30000 } } };
 var bodyParser = require("body-parser");
 var multer  = require('multer');
 var upload = multer({ dest: 'uploads/' });
-
+var lat;
+var lng;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -19,7 +20,9 @@ var customerschema = mongoose.Schema({
   modele: String,
   marque: String,
   ville: String,
-  places: String
+  places: String,
+  latitude: String,
+  longitude: String
 });
 
 var customermodel = mongoose.model("customers", customerschema);
@@ -39,17 +42,25 @@ app.get("/", function(req, res) {
 });
 
 app.post("/signup", upload.array(), function(req, res) {
-  console.log(req.body);
-  var newcustomer = new customermodel({
-    modele: req.body.modele,
-    marque: req.body.marque,
-    ville: req.body.marque,
-    places: req.body.places
+  request(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.city}&key=AIzaSyDZFJG4GhBshMDF2oz93IfAkf8oYHIx6c4`, function(error, response, body){
+    var retourapi = JSON.parse(body);
+    console.log(retourapi);
+    lat = body.results[0].geometry.location.lat;
+    lng = body.results[0].geometry.location.lng;
+    console.log(req.body);
+    var newcustomer = new customermodel({
+      modele: req.body.modele,
+      marque: req.body.marque,
+      ville: req.body.marque,
+      places: req.body.places,
+      latitude: lat,
+      longitude: lng
+    });
+    newcustomer.save(function(error, customers) {
+      ///on peut mettre une fonction de call-back ici
+    });
+    res.redirect("/");
   });
-  newcustomer.save(function(error, customers) {
-    ///on peut mettre une fonction de call-back ici
-  });
-  res.redirect("/");
 });
 
 ///***---LISTEN---***\\\
