a1b30bdcb7c6cf6fd51e679ecde21eecbfcbc50b
Allan Prost <prost.allan89@gmail.com>
2017-12-11 17:48:26 +0100

    dfgfdg

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.post("/signup", function(req, res) {
         console.log(customers);
         console.log(customers._id);
         ///on peut mettre une fonction de call-back ici
-        res.json({id : customers._id});
+        res.json(customers._id);
       });
     }
   );
