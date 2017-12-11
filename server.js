4a15f60cd0950810594c47dee5d910d07069b855
Allan Prost <prost.allan89@gmail.com>
2017-12-11 14:53:51 +0100

    FKCKJNFS

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-
+app.use(fileUpload());
 
 ///***---MONGODB---***\\\
 var customerschema = mongoose.Schema({
