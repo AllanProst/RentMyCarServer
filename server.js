093a8a23f15a7fc261d1c57b68e56bb293bf0270
Allan Prost <prost.allan89@gmail.com>
2017-12-11 11:28:14 +0100

    caca

--- server.js
+++ server.js
@@ -69,8 +69,8 @@ app.post("/signup", upload.array(), function(req, res) {
   });
 });
 
-app.post("/saveimage", upload.single(), function(req, res) {
-    console.log(imgcar);
+app.post("/saveimage", upload.single("imgcar"), function(req, res) {
+    console.log(req.file);
     res.redirect("/");
 });
 
