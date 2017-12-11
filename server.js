e10285af5eec28e409339f0eedc2f6230e0f6731
Allan Prost <prost.allan89@gmail.com>
2017-12-11 12:32:57 +0100

    finaleeeeee

--- server.js
+++ server.js
@@ -10,11 +10,14 @@ var bodyParser = require("body-parser");
 //// Module pour comprendre les envois "Multipart Formdata"
 var multer  = require('multer');
 var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
   filename: function (req, file, cb) {
     cb(null, "image.jpg")
   }
 })
-var upload = multer({ dest: 'uploads/', storage: storage });
+var upload = multer({ storage: storage });
 
 
 
