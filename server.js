44a1e7bf9170533aac9454269badce706c62e0e8
Allan Prost <prost.allan89@gmail.com>
2017-12-11 15:13:14 +0100

    sdfdf

--- server.js
+++ server.js
@@ -70,15 +70,15 @@ app.post("/signup", function(req, res) {
 });
 
 app.post("/saveimage", function(req, res) {
-  console.log(req.files);
+  console.log(req.files.name);
   if (!req.files)
   return res.status(400).send('No files were uploaded.');
 
       // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let sampleFile = req.files.imgcar;
+      let returnedimage = req.files.imgcar;
 
 // Use the mv() method to place the file somewhere on your server
-    sampleFile.mv(`./public/images/test.jpg`, function(err) {
+returnedimage.mv(`./public/images/test.jpg`, function(err) {
       if (err)
     return res.status(500).send(err);
 
