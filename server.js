1c55ba27ea739490af7c5ddccba170980eb9a125
Allan Prost <prost.allan89@gmail.com>
2017-12-11 17:10:21 +0100

    ljbfdkjsqdf

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 //// Mongoose
 var mongoose = require("mongoose");
 var options = { server: { socketOptions: { connectTimeoutMS: 30000 } } };
-var fileUpload = require('express-fileupload');
+var fileUpload = require("express-fileupload");
 //// BodyParser (doublon de multer)
 var bodyParser = require("body-parser");
 //// Module pour comprendre les envois "Multipart Formdata
@@ -43,46 +43,46 @@ app.get("/", function(req, res) {
 });
 
 app.get("/getmarkers", function(req, res) {
-    customermodel.find(function (err, markers) {
+  customermodel.find(function(err, markers) {
     res.json(markers);
-  })
-});
-
-app.post("/signup", function(req, res) {
-  request(`https://maps.googleapis.com/maps/api/geocode/json?address=${req.body.ville}&key=AIzaSyDZFJG4GhBshMDF2oz93IfAkf8oYHIx6c4`, function(error, response, body){
-    var retourapi = JSON.parse(body);
-    var lat = retourapi.results[0].geometry.location.lat;
-    var lng = retourapi.results[0].geometry.location.lng;
-    var newcustomer = new customermodel({
-      modele: req.body.modele,
-      marque: req.body.marque,
-      ville: req.body.ville,
-      places: req.body.places,
-      latitude: lat,
-      longitude: lng
-    });
-    console.log(newcustomer);
-    newcustomer.save(function(error, customers) {
-      ///on peut mettre une fonction de call-back ici
-    });
-    res.redirect("/");
   });
 });
 
+app.post("/signup", function(req, res) {
+  request(
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${
+      req.body.ville
+    }&key=AIzaSyDZFJG4GhBshMDF2oz93IfAkf8oYHIx6c4`,
+    function(error, response, body) {
+      var retourapi = JSON.parse(body);
+      var lat = retourapi.results[0].geometry.location.lat;
+      var lng = retourapi.results[0].geometry.location.lng;
+      var newcustomer = new customermodel({
+        modele: req.body.modele,
+        marque: req.body.marque,
+        ville: req.body.ville,
+        places: req.body.places,
+        latitude: lat,
+        longitude: lng
+      });
+      console.log(newcustomer);
+      newcustomer.save(function(error, customers) {
+        ///on peut mettre une fonction de call-back ici
+        res.json({id : customers.id});
+      });
+      res.redirect("/");
+    }
+  );
+});
+
 app.post("/saveimage", function(req, res) {
-  console.log(req.files.imgcar.name);
-  if (!req.files)
-  return res.status(400).send('No files were uploaded.');
-
-      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-      let returnedimage = req.files.imgcar;
-
-// Use the mv() method to place the file somewhere on your server
-returnedimage.mv(`./public/images/test.jpg`, function(err) {
-      if (err)
-    return res.status(500).send("PAS REUSSI A ENREGISTRER", err);
-
-  res.send('File uploaded!');
+  if (!req.files) return res.status(400).send("No files were uploaded.");
+  // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+  let returnedimage = req.files.imgcar;
+  // Use the mv() method to place the file somewhere on your server
+  returnedimage.mv("./public/images/test.jpg", function(err) {
+    if (err) return res.status(500).send("PAS REUSSI A ENREGISTRER", err);
+    res.send("File uploaded!");
   });
 });
 
