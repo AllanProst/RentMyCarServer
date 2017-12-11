b6ae1629382b8e4257cf7ecf2708cf13c313d18d
Allan Prost <prost.allan89@gmail.com>
2017-12-11 11:41:11 +0100

    final

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 ///***---MONGODB---***\\\
 var customerschema = mongoose.Schema({
