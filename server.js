814deb44d35bcbe2bdb61c965539ecd120e96e84
Allan Prost <prost.allan89@gmail.com>
2017-12-12 10:15:30 +0100

    gfd

--- server.js
+++ server.js
@@ -78,6 +78,7 @@ app.post("/saveimage", function(req, res) {
   if (!req.files) return res.status(400).send("No files were uploaded.");
   // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
   let returnedimage = req.files.imgcar;
+  console.log(`./public/images/${returnedimage.name}.jpg`);
   // Use the mv() method to place the file somewhere on your server
   returnedimage.mv(`./public/images/${returnedimage.name}.jpg`, function(err) {
     if (err) return res.status(500).send("PAS REUSSI A ENREGISTRER", err);
